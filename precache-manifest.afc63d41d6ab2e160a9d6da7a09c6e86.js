1a416097d584a6d773988040fc94126d5678aaf4
Mona Lisa <david.t.bui83@gmail.com>
2020-07-23 16:48:52 -0700

    Updates

---
+++ precache-manifest.afc63d41d6ab2e160a9d6da7a09c6e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4414e58497cfe0d9e885435e8501a40",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c94b9da4b60957b64e2b",
+    "url": "/monsters/static/css/main.ae25535b.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monsters/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94b9da4b60957b64e2b",
+    "url": "/monsters/static/js/main.430cc26b.chunk.js"
+  },
+  {
+    "revision": "7d11275995119a716e88",
+    "url": "/monsters/static/js/runtime-main.5df1f7b5.js"
+  }
+]);
